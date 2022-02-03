4ceb511c139e0503cbd080d033c7c670f12ae202
Duane Kord <duanekord@gmail.com>
2022-02-03 01:11:03 -0500

    interactions working

--- src/controllers/Interactions.ts
+++ src/controllers/Interactions.ts
@@ -1,123 +1,150 @@
-import { Client, Interaction, Message, MessageActionRow, MessageButton, MessageEmbed, User, TextChannel, ChannelManager } from "discord.js";
+/* eslint-disable prefer-const */
+/* eslint-disable sort-keys */
+import { Client, Interaction, MessageActionRow, MessageButton, MessageEmbed, TextChannel } from "discord.js";
 import { DateTime } from "luxon";
 import LeaderboardRepository from "../repositories/LeaderboardRepository";
 import { PlayerStats } from "../repositories/LeaderboardRepository/types";
 import QueueRepository from "../repositories/QueueRepository";
-import { BallChaser } from '../types/common';
+import { BallChaser } from "../types/common";
 const NormClient = new Client({ intents: "GUILDS" });
 
 
 export async function buttonEmbeds(queueChannel : TextChannel): Promise<void> {
 
-    const row1 = new MessageActionRow()
-        .addComponents(
-            new MessageButton()
-                .setCustomId('joinQueue')
-                .setLabel('Join')
-                .setStyle('SUCCESS'),
-            new MessageButton()
-                .setCustomId('leaveQueue')
-                .setLabel('Leave')
-                .setStyle('DANGER'),
-        );
+  let ballchasers = await QueueRepository.getAllBallChasersInQueue();
 
+  const row1 = new MessageActionRow()
+    .addComponents(
+      new MessageButton()
+        .setCustomId("joinQueue")
+        .setLabel("Join")
+        .setStyle("SUCCESS"),
+      new MessageButton()
+        .setCustomId("leaveQueue")
+        .setLabel("Leave")
+        .setStyle("DANGER"),
+    );
+
+  if (ballchasers == null) {
     const embed = new MessageEmbed()
-        .setColor('#3ba55c') // <- This is green
-        .setTitle('Queue is Empty')
-        .setDescription('Click the green button to join the queue!')
+      .setColor("#3ba55c") // <- This is green
+      .setTitle("Queue is Empty")
+      .setDescription("Click the green button to join the queue!");
     console.info("NormJS is running.");
 
     await queueChannel.send({ embeds: [embed], components: [row1] });
-};
+
+  } else {
+    let ballChaserNames = ballchasers.map(function(a) {return a.name;});
+
+    const embed = new MessageEmbed()
+      .setColor("#3ba55c") // <- This is green
+      .setTitle("Current Queue")
+      .setDescription("Click the green button to join the queue!\n\n" + ballChaserNames.join("\n\n"));
+    console.info("NormJS is running.");
+
+    await queueChannel.send({ embeds: [embed], components: [row1] });
+  }   
+}
+
+NormClient.on("interactionCreate", async (buttonInteraction: Interaction) => {
+
+  if (!buttonInteraction.isButton()) return;
+  console.log(buttonInteraction);
+
+  switch (buttonInteraction.customId) {
+    case "joinQueue": {
+
+      let queueMember: BallChaser | null;
+      queueMember = await QueueRepository.getBallChaserInQueue(buttonInteraction.user.toString());
+
+      let leaderboardMember: Readonly<PlayerStats> | null;
+      leaderboardMember = await LeaderboardRepository.getPlayerStats(buttonInteraction.user.toString());
+
+      if (queueMember == null && leaderboardMember == null) {
+        const player: BallChaser = {
+          id: buttonInteraction.user.toString(),
+          mmr: 100,
+          name: buttonInteraction.user.username,
+          isCap: false,
+          team: null,
+          queueTime: DateTime.now(),
+        };
+        
+        await QueueRepository.addBallChaserToQueue(player);
+      }
+      
+      if (queueMember == null && leaderboardMember != null) {
+        const player: BallChaser = {
+          id: buttonInteraction.user.toString(),
+          mmr: leaderboardMember.mmr,
+          name: buttonInteraction.user.username,
+          isCap: false,
+          team: null,
+          queueTime: DateTime.now(),
+        };
+
+        await QueueRepository.addBallChaserToQueue(player);
+      }
+
+      const row1 = new MessageActionRow()
+        .addComponents(
+          new MessageButton()
+            .setCustomId("joinQueue")
+            .setLabel("Join")
+            .setStyle("SUCCESS"),
+          //.setDisabled(Need a method that returns T/F to determine if this is a clickable button after a full queue)
+          new MessageButton()
+            .setCustomId("leaveQueue")
+            .setLabel("Leave")
+            .setStyle("DANGER"),
+        );
 
 
-NormClient.on('interactionCreate', async (buttonInteraction: Interaction) => {
+      let ballchasers = await QueueRepository.getAllBallChasersInQueue();
+      let ballChaserNames = ballchasers.map(function(a) {return a.name;});
+       
+      const embed = new MessageEmbed()
+        .setColor("#3ba55c") // <- This is green
+        .setTitle(buttonInteraction.user.username + " Joined the Queue!")
+        .setDescription("Click the green button to join the queue!\n\n" + ballChaserNames.join("\n\n"));
 
-    if (!buttonInteraction.isButton()) return;
-    console.log(buttonInteraction);
-
-    switch (buttonInteraction.customId) {
-        case 'joinQueue': {
-
-            let member: BallChaser | null;
-            member = await QueueRepository.getBallChaserInQueue(buttonInteraction.user.toString());
-            if (member == null) {
-                const player: BallChaser = {
-                    id: buttonInteraction.user.toString(),
-                    mmr: 100,
-                    name: buttonInteraction.user.username,
-                    isCap: false,
-                    team: null,
-                    queueTime: DateTime.now(),
-                }
-                console.log("Add BallChaser 2")
-                await QueueRepository.addBallChaserToQueue(player);
-
-            } else {
-                QueueRepository.addBallChaserToQueue(member);
-            }
-
-            const row1 = new MessageActionRow()
-                .addComponents(
-                    new MessageButton()
-                        .setCustomId('joinQueue')
-                        .setLabel('Join')
-                        .setStyle('SUCCESS'),
-                    //.setDisabled(Need a method that returns T/F to determine if this is a clickable button after a full queue)
-                    new MessageButton()
-                        .setCustomId('leaveQueue')
-                        .setLabel('Leave')
-                        .setStyle('DANGER'),
-                );
-
-
-            let ballchasers = await QueueRepository.getAllBallChasersInQueue();
-            console.log(ballchasers);
-            // let names = [] 
-            // for (let i = 0; i <= ballchasers.length; i++) {
-            //     names[i] = ballchasers[i].name;
-            // }
-            
-            const embed = new MessageEmbed()
-                .setColor('#3ba55c') // <- This is green
-                .setTitle(buttonInteraction.user.username + ' Joined the Queue!')
-                .setDescription('Click the green button to join the queue!\n\n' +
-                    ballchasers[0].name + '\n\n' + ballchasers[1].name);
-
-            await buttonInteraction.update({ embeds: [embed], components: [row1] })
-            break;
-        }
-
-        case 'leaveQueue': {
-
-            let member: BallChaser | null;
-            member = await QueueRepository.getBallChaserInQueue(buttonInteraction.user.toString());
-            if (member != null) {
-                QueueRepository.removeBallChaserFromQueue(buttonInteraction.user.toString());
-            }
-
-            const row1 = new MessageActionRow()
-                .addComponents(
-                    new MessageButton()
-                        .setCustomId('joinQueue')
-                        .setLabel('Join')
-                        .setStyle('SUCCESS'),
-                    new MessageButton()
-                        .setCustomId('leaveQueue')
-                        .setLabel('Leave')
-                        .setStyle('DANGER'),
-                );
-
-            const embed = new MessageEmbed()
-                .setColor('#ed4245') // <- This is red
-                .setTitle(buttonInteraction.user.username + ' Left the Queue!')
-                .setDescription('Click the green button to join the queue!\n\n' +
-                    await QueueRepository.getAllBallChasersInQueue());
-
-            await buttonInteraction.update({ embeds: [embed], components: [row1] })
-            break;
-        }
+      await buttonInteraction.update({ embeds: [embed], components: [row1] });
+      break;
     }
-})
+
+    case "leaveQueue": {
+
+      let member: BallChaser | null;
+      member = await QueueRepository.getBallChaserInQueue(buttonInteraction.user.toString());
+
+      if (member != null) {
+        let remainingMembers = await QueueRepository.removeBallChaserFromQueue(buttonInteraction.user.toString());
+
+        const row1 = new MessageActionRow()
+          .addComponents(
+            new MessageButton()
+              .setCustomId("joinQueue")
+              .setLabel("Join")
+              .setStyle("SUCCESS"),
+            new MessageButton()
+              .setCustomId("leaveQueue")
+              .setLabel("Leave")
+              .setStyle("DANGER"),
+          );
+
+        let ballChaserNames = remainingMembers.map(function(a) {return a.name;});
+        
+        const embed = new MessageEmbed()
+          .setColor("#ed4245") // <- This is red
+          .setTitle(buttonInteraction.user.username + " Left the Queue!")
+          .setDescription("Click the green button to join the queue!\n\n" + ballChaserNames.join("\n\n"));
+
+        await buttonInteraction.update({ embeds: [embed], components: [row1] });
+      }
+      break;
+    }
+  }
+});
 
 NormClient.login(process.env.token);
